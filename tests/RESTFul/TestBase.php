8ead9dfe7c30d3daf7491debd19b5c555ae1df10
Raaghu <krraghavendra@yahoo.co.in>
2018-02-09 20:55:23 +0530

    included coverage for tests run in php-cli process

--- tests/RESTFul/TestBase.php
+++ tests/RESTFul/TestBase.php
@@ -9,9 +9,18 @@ use PhpPlatform\RESTFul\Routing\Build;
 abstract class TestBase extends \PHPUnit_Framework_TestCase {
 	
 	private static $errorLogDir = null;
+	private static $coverage = null;
 	
 	static function setUpBeforeClass(){
 		parent::setUpBeforeClass();
+		
+		// generate coverage for tests run in php-cli process
+		if(defined('COVERAGE_DIR')){
+			$filter = new \PHP_CodeCoverage_Filter();
+			$filter->addDirectoryToWhitelist(dirname(__FILE__).'/../../src');
+			self::$coverage = new \PHP_CodeCoverage(null,$filter);
+			self::$coverage->start('testiCircleAccounts');
+		}
 		
 		// create a temporary error log directory
 		$errorLogDir = sys_get_temp_dir().'/php-platform/restful/errors/'.microtime(true);
@@ -23,28 +32,12 @@ abstract class TestBase extends \PHPUnit_Framework_TestCase {
 		// clear caches
 		SettingsCache::getInstance()->reset();
 		
-		// build the routes - with coverage
-		if(defined('APP_COVERAGE') && APP_COVERAGE == "true"){
-			$filter = new \PHP_CodeCoverage_Filter();
-			$filter->addDirectoryToWhitelist(dirname(__FILE__).'/../../src');
-			
-			$coverage = new \PHP_CodeCoverage(null,$filter);
-			$coverage->start('testRESTful');
-		}
-		
 		Build::run();
 		
 		/**
 		 * @desc HACK : same file is used by SettingsCache , tests are run from root user and apache is run from www-data , causing permission issues to access this shared cache file
 		 */
 		chmod(sys_get_temp_dir().'/settingscache236512233125', 0777);
-		
-		if(isset($coverage)){
-			$coverage->stop();
-			$writer = new \PHP_CodeCoverage_Report_PHP();
-			$coverageFileName = 'coverage_build.php';
-			$writer->process($coverage, COVERAGE_DIR.'/'.$coverageFileName);
-		}
 	}
 	
 	function setUp(){
@@ -88,6 +81,13 @@ abstract class TestBase extends \PHPUnit_Framework_TestCase {
 	static function tearDownAfterClass(){
 		// delete error log directory
 		rmdir(self::$errorLogDir);
+		
+		if(defined('COVERAGE_DIR')){
+			self::$coverage->stop();
+			$writer = new \PHP_CodeCoverage_Report_PHP();
+			$coverageFileName = md5(microtime()).'.php';
+			$writer->process(self::$coverage, COVERAGE_DIR.'/'.$coverageFileName);
+		}
 	}
 	
 }
